8b212305f7ab20739d8878cef6f598d69a66e43f
kaisarnur <itmkaisar@gmail.com>
2021-12-20 20:37:13 +0600

    #13 Изменил базу данных

--- source/Loyalty_Program_App/settings.py
+++ source/Loyalty_Program_App/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Loyalty_Program_App.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'kaisar',
+        'PASSWORD': 'Kaisar_99',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
