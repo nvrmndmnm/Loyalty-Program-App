9ddad48a5bd9cff226238a6afe4b1abdeee73c4f
Said <s.avkhadeev@gmail.com>
2021-12-24 02:41:17 +0600

    Слил ветки по работе с ботом

--- LoyaltyBot/config.py
+++ LoyaltyBot/config.py
@@ -1,5 +1,1 @@
 TG_TOKEN = '5018591042:AAF7puGCIWgLn4vAL9mb4KMfsz9pYV0Yy3M'
-
-# import telegram
-# bot = telegram.Bot(token='5018591042:AAF7puGCIWgLn4vAL9mb4KMfsz9pYV0Yy3M')
-
--- LoyaltyBot/main.py
+++ LoyaltyBot/main.py
@@ -5,9 +5,9 @@ from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 from telegram.ext import CallbackContext
 
-from uplink import Consumer ,returns, get
+from uplink import Consumer, returns, get
 
-from buttons import *
+from buttons import get_register_keyboard, get_base_reply_keyboard
 
 from config import TG_TOKEN
 import requests
@@ -44,10 +44,9 @@ def establishments(update: Update, context: CallbackContext):
     branches = ''
     for i in loyaltyapi.get_repos('branches'):
         branches += f'Название {i["name"]}\n' \
-                    f'Aдресс: {i["address"]}\n' \
+                    f'Aдрес: {i["address"]}\n' \
                     f'{i["description"]}\n\n'
     context.bot.send_message(chat_id=update.effective_chat.id, text=branches,)
-
 
 
 def register(update: Update, context: CallbackContext):
@@ -95,11 +94,13 @@ def main():
 
     updater.idle()
 
+
 class LoyaltyApi(Consumer):
     @returns.json
     @get("{path}/")
     def get_repos(self, path):
         pass
+
 
 loyaltyapi = LoyaltyApi(base_url="http://localhost:8000/api/")
 
