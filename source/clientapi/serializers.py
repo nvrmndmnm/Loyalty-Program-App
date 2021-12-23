4fa067003ae21d614955d96b085a12a061992d5d
Said <s.avkhadeev@gmail.com>
2021-12-24 02:22:43 +0600

    #31 Добавил регистрацию пользователя в Telegram

--- source/clientapi/serializers.py
+++ source/clientapi/serializers.py
@@ -1,5 +1,12 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from merchantapp.models import Branch
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('phone', 'tg_id')
 
 
 class BranchWriteSerializer(serializers.ModelSerializer):
