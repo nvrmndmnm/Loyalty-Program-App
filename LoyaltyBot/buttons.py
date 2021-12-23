b694e2ec376a4bc8d039ca169aa491053d9feba3
Said <s.avkhadeev@gmail.com>
2021-12-24 02:23:56 +0600

    #4 Добавил регистрацию пользователей в боте

--- LoyaltyBot/buttons.py
+++ LoyaltyBot/buttons.py
@@ -6,14 +6,22 @@ BUTTON2_NEWS = 'Новости'
 BUTTON3_AWARDS = 'Мои награды'
 BUTTON4_SETTINGS = 'Настройки'
 BUTTON5_QR = 'QR код'
+BUTTON6_REGISTER = 'Авторизация'
 
+
+def get_register_keyboard():
+    keyboard = [[KeyboardButton(BUTTON6_REGISTER, request_contact=True)]]
+    return ReplyKeyboardMarkup(
+        keyboard=keyboard,
+        resize_keyboard=True,
+    )
 
 
 def get_base_reply_keyboard():
-    keyboard =[
-          [KeyboardButton(BUTTON1_ESTABLISHMENTS), KeyboardButton(BUTTON2_NEWS)],
-          [KeyboardButton(BUTTON3_AWARDS), KeyboardButton(BUTTON4_SETTINGS)],
-          [KeyboardButton(BUTTON5_QR)],
+    keyboard = [
+        [KeyboardButton(BUTTON1_ESTABLISHMENTS), KeyboardButton(BUTTON2_NEWS)],
+        [KeyboardButton(BUTTON3_AWARDS), KeyboardButton(BUTTON4_SETTINGS)],
+        [KeyboardButton(BUTTON5_QR, request_contact=True)]
     ]
     return ReplyKeyboardMarkup(
         keyboard=keyboard,
--- LoyaltyBot/main.py
+++ LoyaltyBot/main.py
@@ -8,15 +8,31 @@ from telegram.ext import CallbackContext
 from buttons import *
 
 from config import TG_TOKEN
+import requests
+
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('Добро пожаловать в нашего бота-шмота! Пожалуйста, авторизуйтесь для продолжения.',
+                              reply_markup=get_register_keyboard())
 
 
 def about(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Приложение LoyaltyBot позволяет получать бесплатные товары за покупки",reply_markup=get_base_reply_keyboard())
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text=do_echo(update, context),
+                             reply_markup=get_register_keyboard()
+                             )
 
 
 def help(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Для получения бонусов просто приходите в заведение и сканируйте QR код")
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text=sendRequest(),
+                             reply_markup=get_register_keyboard()
+                             )
 
+
+def sendRequest():
+    response = requests.get('http://localhost:8002/api/branches/').json()
+    return response
 # def echo(update: Update, context: CallbackContext):
 #     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text,)
 
@@ -26,12 +42,33 @@ def establishments(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text='Здесь будут отображаться все заведения',)
 
 
+def register(update: Update, context: CallbackContext):
+    if update.message.contact.user_id == update.message.chat_id:
+        url = 'http://localhost:8002/api/users/create/'
+        data = {'phone': update.message.contact.phone_number.replace('+', ''),
+                'tg_id': update.message.contact.user_id}
+        response = requests.post(url, data=data)
+        print(response.text)
+        resp = response.text
+    else:
+        resp = 'Пожалуйста, укажите данные своего аккаунта.'
+    context.bot.send_message(chat_id=update.effective_chat.id, text=resp, reply_markup=get_base_reply_keyboard())
+
+
+def do_echo(update: Update, context: CallbackContext):
+    chat_id = update.message.chat_id
+    text = update.message.text
+    update.message.reply_text(
+        text=f"Yo {text}"
+    )
+
+
 def main():
     bot = Bot(
         token=TG_TOKEN,
     )
     updater = Updater(TG_TOKEN, use_context=True)
-
+    updater.dispatcher.add_handler(CommandHandler('start', start))
     about_handler = CommandHandler('about', about)
     help_handler = CommandHandler('help', help)
     # echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
@@ -43,9 +80,25 @@ def main():
     establishments_handler = MessageHandler(Filters.text & (~Filters.command), establishments)
     updater.dispatcher.add_handler(establishments_handler)
 
+    register_handler = MessageHandler(Filters.contact & (~Filters.command), register)
+    updater.dispatcher.add_handler(register_handler)
+
     updater.start_polling()
+
     updater.idle()
 
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     main()
+
+
+def build_menu(buttons, n_cols,
+               header_buttons=None,
+               footer_buttons=None):
+    print(buttons)
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, [header_buttons])
+    if footer_buttons:
+        menu.append([footer_buttons])
+    return menu
