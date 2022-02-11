b7c8745b419c5a7d45475f52234db81afa3bc460
skkv7 <dusha.skokov2001@gmail.com>
2022-02-11 20:16:49 +0700

    * #55 Добавил обработку ошибок API для телеграмм бота

--- LoyaltyBot/source/buttons.py
+++ LoyaltyBot/source/buttons.py
@@ -6,7 +6,7 @@ BUTTON2_NEWS = 'Новости'
 BUTTON3_AWARDS = 'Мои награды'
 BUTTON4_SETTINGS = 'Настройки'
 BUTTON5_QR = 'QR код'
-BUTTON6_REGISTER = 'Авторизация'
+BUTTON6_REGISTER = 'Авторизацияяяя'
 
 
 def get_register_keyboard():
