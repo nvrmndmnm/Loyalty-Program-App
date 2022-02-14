5d2bba542f342ad0ea6a7e3c2288ba6549c57d10
Said <s.avkhadeev@gmail.com>
2022-02-15 02:05:10 +0600

    #68 Создал функционал генерации паролей пользователей

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from django.forms import widgets
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class UserAdminCreationForm(UserCreationForm):
@@ -48,3 +48,15 @@ class PasswordChangeForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ["old_password", "password", "password_confirm"]
+
+
+class PasswordRequestForm(forms.Form):
+    phone = UsernameField(widget=forms.TextInput(attrs={'autofocus': True}), label='Номер телефона')
+
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        try:
+            get_user_model().objects.get(phone=phone)
+        except ObjectDoesNotExist:
+            raise forms.ValidationError('User not found')
+        return phone
--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetDoneView, PasswordResetConfirmView, \
+    PasswordResetCompleteView
+from django.urls import path, reverse_lazy
 from accounts import views as accounts_views
 
 app_name = 'accounts'
@@ -10,5 +11,15 @@ urlpatterns = [
     path("create/", accounts_views.RegisterView.as_view(), name="register"),
     path("<int:pk>/", accounts_views.UserDetailsView.as_view(), name="profile"),
     path("<int:pk>/edit/", accounts_views.UserUpdateView.as_view(), name="edit_profile"),
-    path("<int:pk>/change-password/", accounts_views.ChangePasswordView.as_view(), name="change_password")
+    path("<int:pk>/change-password/", accounts_views.ChangePasswordView.as_view(), name="change_password"),
+    path('password_reset/done/',
+         PasswordResetDoneView.as_view(template_name='password/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(template_name="password/password_reset_confirm.html",
+                                          success_url=reverse_lazy('accounts:password_reset_complete')),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         PasswordResetCompleteView.as_view(template_name='password/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
