b47b56fd03f43d1afb32f26a9952374c95c315b9
Said <s.avkhadeev@gmail.com>
2022-03-06 20:44:25 +0600

    #77 Разделил обработку заказов по мёрчантам

--- source/merchantapp/templatetags/merchant_tags.py
+++ source/merchantapp/templatetags/merchant_tags.py
@@ -1,7 +1,22 @@
 from django import template
+from django.db.models import Q
+
+from merchantapp.models import Merchant, Branch, Program
+
 register = template.Library()
 
 
 @register.filter
-def redeemable_count(rewards):
-    return rewards.filter(redeemed=False).count()
+def redeemable_count(rewards, merchant_employee):
+    merchant = Merchant.objects.filter(Q(director=merchant_employee) | Q(employees=merchant_employee)).first()
+    branches = Branch.objects.filter(merchant=merchant)
+    programs = Program.objects.filter(branch__in=branches)
+    return rewards.filter(redeemed=False, program__in=programs).count()
+
+
+@register.filter
+def total_count(objects, merchant_employee):
+    merchant = Merchant.objects.filter(Q(director=merchant_employee) | Q(employees=merchant_employee)).first()
+    branches = Branch.objects.filter(merchant=merchant)
+    programs = Program.objects.filter(branch__in=branches)
+    return objects.filter(program__in=programs).count()
